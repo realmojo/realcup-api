65b806d0a2aa1fa36d4a62b4ade806a1930e56a3
realmojo <strikers1999@naver.com>
2022-10-17 01:38:03 +0900

    update

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,23 +1,9 @@
-// import { Controller, Get } from '@nestjs/common';
-// import { AppService } from './app.service';
-
-// @Controller()
-// export class AppController {
-//   constructor(private readonly appService: AppService) {}
-
-//   @Get()
-//   getHello(): string {
-//     return this.appService.getHello();
-//   }
-// }
-
 import {
   Controller,
   Get,
   Request,
   Post,
   UseGuards,
-  Param,
   Body,
 } from '@nestjs/common';
 import { JwtAuthGuard } from './auth/jwt-auth.guard';
